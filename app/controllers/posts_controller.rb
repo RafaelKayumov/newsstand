6bc93273f5e706de0da2e4048e33016efade59b5
Rafael Kayumov <justpoc@gmail.com>
2015-04-23 15:51:57 +0300

    Double sort by date and rating

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,15 @@ class PostsController < ApplicationController
   def show
   	@post = Post.find(params[:id])
   end
-  
+
   def index
-  	@posts = Post.all
+  	#@posts = Post.all.sort_by{ |post| [post[:rating], post[:created_at]]}
+  	@posts = Post.all.sort do |a,b|
+  		comparsion = a.rating <=> b.rating
+  		if comparsion == 0 
+  			b.created_at <=> a.created_at
+  		end
+  	end
   end
 
   def new
