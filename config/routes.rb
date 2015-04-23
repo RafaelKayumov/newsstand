e05965c281e0637e44a973b7e9dc7cf7fbdc0487
Rafael Kayumov <justpoc@gmail.com>
2015-04-23 15:01:38 +0300

    Create and display new post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'posts/index'
 
+  resources :posts
+
   root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
