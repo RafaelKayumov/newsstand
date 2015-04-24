af26a3eb1b0942b4135f3ea19502911ca49834df
Rafael Kayumov <justpoc@gmail.com>
2015-04-24 17:49:08 +0300

    Added edit method and form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,20 +2,20 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
 
   def show
-  	@post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def index
-  	@posts = Post.all.sort do |a,b|
-  		comparsion = a.rating <=> b.rating
-  		if comparsion == 0 
-  			b.created_at <=> a.created_at
-  		end
-  	end
+    @posts = Post.all.sort do |a,b|
+      comparsion = a.rating <=> b.rating
+      if comparsion == 0 
+        b.created_at <=> a.created_at
+      end
+    end
   end
 
   def new
-  	@post = Post.new
+    @post = Post.new
     @post.user_id = current_user.id
   end
 
@@ -31,8 +31,21 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
   private
   def post_params
-  	params.require(:post).permit(:title, :text)
+    params.require(:post).permit(:title, :text)
   end
 end
