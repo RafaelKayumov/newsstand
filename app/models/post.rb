b9613e68625b30455445198d8ebcb58b61c12976
Rafael Kayumov <justpoc@gmail.com>
2015-05-07 15:56:32 +0300

    Replaces unnecessary method with delegate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,12 @@
 class Post < ActiveRecord::Base
   belongs_to :user, inverse_of: :posts
   has_many :votes, dependent: :destroy, inverse_of: :post
+  delegate :email, to: :user
 
   validates :title, presence: true
   validates :text, presence: true
 
   scope :foremost, -> { order('rating DESC, created_at DESC') }
-
-  def user_email
-    self.user.email
-  end
 
   def can_moderate(user)
     self.user_id == user.try(:id)
